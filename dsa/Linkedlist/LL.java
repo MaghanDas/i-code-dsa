382ace24c2a7bca0fc1cb2ca37ec85319f8bb13c
maghandas <dasmaghan786@gmail.com>
2023-12-12 14:47:59 +0500

    linkedlist updated

--- dsa/Linkedlist/LL.java
+++ dsa/Linkedlist/LL.java
@@ -3,10 +3,10 @@ package dsa.Linkedlist;
 public class LL {
   public static void main(String[] args) {
 
-    Node head = new Node(1);
-    head.next = new Node(2);
-    head.next.next = new Node(3);
-    head.next.next.next = new Node(4);
+    Node head = new Node(7);
+    head.next = new Node(1);
+    head.next.next = new Node(7);
+    head.next.next.next = new Node(7);
 
     // methods on linkedlist...
     // print the list
@@ -16,7 +16,7 @@ public class LL {
     // add the elements
 //    Node append = head.append(3,head);
 //    append.display(append);
-    Node remove = head.remove(head,4);
+    Node remove = head.remove(head,7);
     remove.display(remove);
 
   }
--- dsa/Linkedlist/Node.java
+++ dsa/Linkedlist/Node.java
@@ -1,65 +1,86 @@
 package dsa.Linkedlist;
 
-import com.github.weisj.jsvg.N;
-
 import java.util.Scanner;
+  public class Node {
+    int value;
+    Node next;
 
-public class Node {
-  int value;
-  Node next;
-  public Node(int value) {
-    this.value = value;
-  }
-  public Node(int value,Node next) {
-    this.value = value;
-    this.next = next;
-  }
-  Scanner sc = new Scanner(System.in);
-  public void display(Node head) {
-    Node temp = head;
-    while ( temp != null ) {
-      System.out.println("elements are "+ temp.value);
-      temp = temp.next;
+    public Node(int value) {
+      this.value = value;
     }
-  }
-  public int sizeOfList(Node head) {
-    int count = 0;
-    Node temp = head;
-    while (temp != null) {
-      count += 1;
-      temp = temp.next;
+
+    public Node(int value, Node next) {
+      this.value = value;
+      this.next = next;
     }
-    return count;
-  }
-  public Node append(int x, Node head) {
-    Node newNode = new Node(x);
-    if (head == null) {
-      head = newNode;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void display(Node head) {
+      Node temp = head;
+      while (temp != null) {
+        System.out.println("elements are " + temp.value);
+        temp = temp.next;
+      }
+    }
+
+    public int sizeOfList(Node head) {
+      int count = 0;
+      Node temp = head;
+      while (temp != null) {
+        count += 1;
+        temp = temp.next;
+      }
+      return count;
+    }
+
+    public Node append(int x, Node head) {
+      Node newNode = new Node(x);
+      if (head == null) {
+        head = newNode;
+        return head;
+      }
+      Node temp = head;
+      while (temp.next != null) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
       return head;
     }
-    Node temp = head;
-    while (temp.next!= null) {
-      temp = temp.next;
-    }
-    temp.next = newNode;
-    return head;
-  }
-  public Node insert(Node head) {
 
-    return head;
-  }
+    public Node insert(Node head) {
 
-  public Node remove(Node head,int x) {
-    Node temp = head;
-    if (head.value == x) {
-      head = head.next;
+      return head;
     }
-    while (temp != null ){
-      if (temp.next.value == x) {
-        temp.next = temp.next.next;
+
+//    public Node remove(Node head, int x) {
+//      Node temp = head;
+//      if (head.value == x) {
+//        head = head.next;
+//      }
+//      while (temp != null) {
+//        if (temp.next.value == x) {
+//          temp.next = temp.next.next;
+//        }
+//        temp = temp.next;
+//      }
+//      return head;
+//    }
+
+    public Node remove(Node head, int x) {
+      if (head.value == x) {
+        head = head.next;
       }
-      temp = temp.next;
+      Node temp = head;
+      while (temp != null) {
+        if (temp.value == x) {
+          if (temp.next.value ==x);
+          temp = temp.next;
+        }
+
+        temp = temp.next;
+      }
+      return head;
     }
-    return head;
   }
-}
+
