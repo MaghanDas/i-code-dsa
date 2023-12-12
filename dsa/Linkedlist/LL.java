4d7c25f2a71af77e06e5d6f0419cab69efcdf15e
maghandas <dasmaghan786@gmail.com>
2023-12-12 13:33:08 +0500

    linkedlist files modified

--- dsa/Linkedlist/LL.java
+++ dsa/Linkedlist/LL.java
@@ -3,5 +3,16 @@ package dsa.Linkedlist;
 public class LL {
   public static void main(String[] args) {
 
+    Node head = new Node(1);
+    head.next = new Node(2);
+
+    // methods on linkedlist...
+    // print the list
+    head.display(head);
+    // size of the list
+    System.out.println(head.sizeOfList(head));
+    // add the elements
+
+
   }
 }
--- dsa/Linkedlist/Node.java
+++ dsa/Linkedlist/Node.java
@@ -1,5 +1,35 @@
 package dsa.Linkedlist;
 
+import java.util.Scanner;
+
 public class Node {
-  
+  int value;
+  Node next;
+  public Node(int value) {
+    this.value = value;
+  }
+  public Node(int value,Node next) {
+    this.value = value;
+    this.next = next;
+  }
+  Scanner sc = new Scanner(System.in);
+  public void display(Node head) {
+    Node temp = head;
+    while ( temp != null ) {
+      System.out.println("elements are "+ temp.value);
+      temp = temp.next;
+    }
+  }
+  public int sizeOfList(Node head) {
+    int count = 0;
+    Node temp = head;
+    while (temp != null) {
+      count += 1;
+      temp = temp.next;
+    }
+    return count;
+  }
+  public Node insert(Node head) {
+    
+  }
 }
