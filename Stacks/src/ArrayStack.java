8e0d1e6c5feb46f5693f1c608b2c1a56905c6ead
maghandas <dasmaghan786@gmail.com>
2023-12-19 11:37:05 +0500

    Added the POP method

--- Stacks/src/ArrayStack.java
+++ Stacks/src/ArrayStack.java
@@ -9,6 +9,27 @@ public class ArrayStack implements Stack{
   public boolean isEmplty() {
     return (size == 0);
   }
+  public Object peek() {
+    if (size ==0 )throw new IllegalStateException("STACK IS EMPTY");
+    return a[size-1];
+  }
+  public Object pop() {
+    if (size ==0 )throw new IllegalStateException("STACK IS EMPTY");
+    Object tmep = a[--size];
+    a[size] = null;
+    return tmep;
+  }
+
+  @Override
+  public void push(Object obj) {
+
+  }
+
+  @Override
+  public int size() {
+    return 0;
+  }
+
   public static void main(String[] args) {
   }
 }
