334592466e1139d3de60c72b52557ed0943d20b2
maghandas <dasmaghan786@gmail.com>
2023-12-20 22:51:39 +0500

    added the linear seach method

---
+++ Searching/src/LinearSearch.java
@@ -0,0 +1,22 @@
+
+// Linear Search
+  public class LinearSearch {
+
+    public static void linearSearch(int[] arr, int target) {
+
+      for (int i = 0; i < arr.length; i++) {
+        if (target == arr[i]) {
+          System.out.println("Element Found at Index : " + i);
+          return;
+        }
+      }
+    }
+
+    public static void main(String[] args) {
+
+      int[] arr = { 1, 2, 3, 4, 5 };
+      int target = 4;
+      LinearSearch.linearSearch(arr, target);
+    }
+  }
+  
