07ebf55a23ad8af15cf72e86bd4fb542395aa447
maghandas <dasmaghan786@gmail.com>
2024-02-05 23:56:35 +0500

    Update Queue.java

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -3,5 +3,10 @@ public class Queue{
   public static void main(String[] args) {
     // queue- follows first in first out approach..
     // queue
+
+    System.out.println("hello world in this world..........");
+
+  
+    
   }
 }
